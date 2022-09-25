d548bce05e72a595e98ffbe5688662b9ad0278f8
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-25 13:27:34 +0300

    added file t47

---
+++ Task_47/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[n,m];
+void Array(double[,] matrix)
+{
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i, j] = new Random().Next(-100,100) * new Random().NextDouble();
+            matrix[i, j] = Math.Round(matrix[i, j], 2);
+            Console.Write(matrix[i, j] + " \t");
+
+        }
+        Console.WriteLine(" \t");
+    }
+}
+
+Array(matrix);
